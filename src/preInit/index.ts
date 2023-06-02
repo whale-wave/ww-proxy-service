232d08dfec5661943178176e298de549858f7e6b
Avan <layouwen@gmail.com>
2023-06-02 16:54:47 +0800

    fix: pkg path error

--- src/preInit/index.ts
+++ src/preInit/index.ts
@@ -2,9 +2,9 @@ import fs from 'fs'
 import path from 'path'
 import config from '../config'
 
-export const DATA_BASE = config.database || path.resolve(__dirname, '../..')
-export const DATABASE_DIR_PATH = path.resolve(DATA_BASE, '.database')
-export const PROXY_FILE_PATH = path.resolve(DATABASE_DIR_PATH, 'proxy.json')
+export const DATA_BASE = config.database || process.cwd()
+export const DATABASE_DIR_PATH = path.join(DATA_BASE, '.database')
+export const PROXY_FILE_PATH = path.join(DATABASE_DIR_PATH, 'proxy.json')
 
 if (!fs.existsSync(DATABASE_DIR_PATH)) {
   fs.mkdirSync(DATABASE_DIR_PATH)
