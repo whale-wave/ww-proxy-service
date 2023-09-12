7df6df7e01660ff2f5a3ac7cb62a9661ea205948
Avan <layouwen@gmail.com>
2023-09-12 09:40:03 +0800

    chore: cors

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,40 @@
-import { forwardMid, proxyMid } from './middleware'
-import express from 'express'
-import config from './config'
-import { initRoute } from './routes'
-import { proxyMapManage, forwardMapManage, logger } from './utils'
-import './preInit'
+import { forwardMid, proxyMid } from "./middleware";
+import express from "express";
+import config from "./config";
+import { initRoute } from "./routes";
+import { proxyMapManage, forwardMapManage, logger } from "./utils";
+import "./preInit";
 // @ts-ignore
-import { expressMid } from 'avan-logger'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import { expressMid } from "avan-logger";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*",
   })
-)
+);
 
-app.use('/api/proxy/to/:alias', proxyMid)
-app.use('/api/forward/to/:alias', forwardMid)
+app.use("/api/proxy/to/:alias", proxyMid);
+app.use("/api/forward/to/:alias", forwardMid);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 if (config.config.logSecret) {
-  app.use(expressMid(logger as any))
+  app.use(expressMid(logger as any));
 }
 
-proxyMapManage.loadCacheData()
-forwardMapManage.loadCacheData()
+proxyMapManage.loadCacheData();
+forwardMapManage.loadCacheData();
 
-initRoute(app)
+initRoute(app);
 
 app.listen(config.port, () => {
   if (config.config.logSecret) {
-    logger.daily.mark('Program start')
-    logger.daily.info('Server running on port', config.port)
+    logger.daily.mark("Program start");
+    logger.daily.info("Server running on port", config.port);
   }
-})
+});
